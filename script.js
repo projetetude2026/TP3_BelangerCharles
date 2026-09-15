486aa538f944cfde0a4958d6ea5e0353521ef137
projetetude2026 <charles.belanger.03@edu.cegeptr.qc.ca>
2026-09-14 20:12:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js")
       .then((registration) => {
         console.log(
           "Service Worker enregistré avec succès:",
